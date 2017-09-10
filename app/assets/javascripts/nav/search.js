7a8fa5518eb7880b67cfa0e5dbdff7b703a2b9c3
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-09-10 10:51:42 -0600

    JS refactors for nav search

--- app/assets/javascripts/nav/search.js
+++ app/assets/javascripts/nav/search.js
@@ -1,22 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
+const appendToList = (data, dataList) => {
+    dataList.innerHTML = ''
+    if (data) {
+        data.forEach((datum) => {
+            var option = document.createElement('option')
+            option.value = datum.name
+            dataList.appendChild(option)
+        });
+    }
+}
+
+const callForList = (e, dataList) => {
+    $.getJSON('http://localhost:3000/api/search', { query: e.currentTarget.value })
+        .then((data) => appendToList(data, dataList))
+}
+
+// $.getJSON('https://corkboard-services.herokuapp.com/api/search', { query: search })
+// .then((data) => appendToList(data, dataList))
+// })
+
+document.addEventListener('DOMContentLoaded', () => {
     const searchInputField = document.getElementById('service_search')
     const dataList = document.getElementById('services_list')
-    if (searchInputField) {
 
-        searchInputField.addEventListener("keyup", function() {
-
-            search = this.value
-            $.getJSON('https://corkboard-services.herokuapp.com/api/search', { query: search },
-                function(data) {
-                    dataList.innerHTML = ''
-                    if (data) {
-                        data.forEach(function(datum) {
-                            var option = document.createElement('option')
-                            option.value = datum.name
-                            dataList.appendChild(option)
-                        });
-                    }
-                })
-        })
-    }
+    if (searchInputField) { searchInputField.addEventListener("keyup", (e) => callForList(e, dataList)) }
 })
